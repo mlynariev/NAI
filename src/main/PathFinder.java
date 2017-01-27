ecd49f0c2b90eadb7372b36353e402d920805bb1
mlynariev <barwoj5@gmail.com>
2017-01-27 15:40:00 +0100

    Added moving cost calculations

--- src/main/PathFinder.java
+++ src/main/PathFinder.java
@@ -3,12 +3,66 @@ package main;
 import java.util.PriorityQueue;
 
 public class PathFinder {
+	public static final int DIAGONAL_COST = 14;
+	public static final int BEELINE_COST = 10;
+	
+	public static Node[][] grid = new Node[5][5];
+	static PriorityQueue<Node> open;
+	
+	public static void aStar(){
+		open.add(grid[Map.startX][Map.startY]);
+		
+		Node current;
+		
+		while(true){
+			current = open.poll();
+			if (current == null) break;
+			Map.isClosed[current.x][current.y]=true;
+			
+			if (current.equals(grid[Map.endX][Map.endY])){
+				return;
+			}
+			
+			Node n;
+			if (current.x-1>=0){
+				n = grid[current.x-1][current.y];
+				Map.calculateMove(current, n, current.fCost+BEELINE_COST);
+				
+				if (current.y-1>=0){
+					n = grid[current.x][current.y-1];
+					Map.calculateMove(current, n, current.fCost+DIAGONAL_COST);
+				}
+				
+				if (current.y+1<grid[0].length){
+					n = grid[current.x-1][current.y+1];
+					Map.calculateMove(current, n, current.fCost+DIAGONAL_COST);
+				}
+			}
+			
+			if (current.y-1>=0){
+				n = grid[current.x][current.y-1];
+				Map.calculateMove(current, n, current.fCost+BEELINE_COST);
+			}
+			
+			if (current.y+1<grid[0].length){
+				n = grid[current.x][current.y+1];
+				Map.calculateMove(current, n, current.fCost+BEELINE_COST);
+			}
+			
+			if(current.x+1<grid.length){
+        n = grid[current.x+1][current.y];
+        Map.calculateMove(current, n, current.fCost+BEELINE_COST); 
 
-	static Node [][] map = new Node[5][5];
-	
-	PriorityQueue<Node> open;
-	
-	boolean isClosed[][] = null;
-	
-	
+        if(current.y-1>=0){
+            n = grid[current.x+1][current.y-1];
+            Map.calculateMove(current, n, current.fCost+DIAGONAL_COST); 
+        }
+        
+        if(current.y+1<grid[0].length){
+           n = grid[current.x+1][current.y+1];
+           Map.calculateMove(current, n, current.fCost+DIAGONAL_COST); 
+        }  
+			}	
+		}
+	}
 }
